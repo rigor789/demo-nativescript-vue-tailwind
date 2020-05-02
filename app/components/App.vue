8f65d149a5cad02f6909493a14de88c37e96a82f
rigor789 <rigor789@gmail.com>
2020-05-02 18:46:47 +0200

    initial commit

---
+++ app/components/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <StackLayout class="space-y-4 divide-y-4 divide-red-500">
+            <Label text="Hello Tailwind!" class="p-4 bg-red-400 bg-opacity-25" />
+            <Label text="Hello Tailwind!" class="p-4 bg-red-400 bg-opacity-50" />
+            <Label text="Hello Tailwind!" class="p-4 bg-red-400 bg-opacity-75" />
+            <Label text="Hello Tailwind!" class="p-4 bg-blue-400 bg-opacity-25" />
+            <Label text="Hello Tailwind!" class="p-4 bg-blue-400 bg-opacity-50" />
+        </StackLayout>
+    </Page>
+</template>
+
+<script >
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './app/**/*.js',
+      './app/**/*.vue'
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
